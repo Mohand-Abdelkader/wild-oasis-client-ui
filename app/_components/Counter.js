9eb88cc53956e8a969f1d1fe369aa5b7ba46c664
Mohand Abdelkader <mohand27m@gmail.com>
2025-05-11 21:50:14 +0300

    setup style and route

---
+++ app/_components/Counter.js
@@ -0,0 +1,13 @@
+"use client";
+import { useState } from "react";
+
+export default function Counter({ users }) {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1>there are {users.length} users</h1>
+      <button onClick={() => setCount((c) => c + 1)}>{count}</button>
+    </div>
+  );
+}
--- app/account/page.js
+++ app/account/page.js
@@ -1,3 +1,10 @@
+export const metadata = {
+  title: "Account Page",
+};
 export default function Page() {
-  return <h1>Welcome to your account</h1>;
+  return (
+    <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+      Welcome Mohand
+    </h2>
+  );
 }
