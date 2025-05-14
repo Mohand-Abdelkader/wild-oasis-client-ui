a82c6a186576bc747c88f84b814d283fa57a3978
Mohand Abdelkader <mohand27m@gmail.com>
2025-05-14 19:50:19 +0300

    update guest profile

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,5 +1,7 @@
 "use server";
-import { signIn, signOut } from "./auth";
+import { revalidatePath } from "next/cache";
+import { auth, signIn, signOut } from "./auth";
+import { supabase } from "./supabase";
 
 export async function signInAction() {
   await signIn("google", { redirectTo: "/account" });
@@ -7,4 +9,28 @@ export async function signInAction() {
 
 export async function signOutAction() {
   await signOut({ redirectTo: "/" });
+}
+
+export async function updateProfile(formData) {
+  const session = await auth();
+  if (!session) throw new Error("you must be logged in");
+
+  const nationalID = formData.get("nationalID");
+  const [nationality, countryFlag] = formData.get("nationality").split("%");
+
+  if (!/^[a-zA-Z0-9]{6,16}$/.test(nationalID))
+    throw new Error("Please Provide a valid National ID");
+
+  const updateData = { nationality, nationalID, countryFlag };
+
+  const { data, error } = await supabase
+    .from("guests")
+    .update(updateData)
+    .eq("id", session.user.guestId);
+
+  if (error) {
+    console.error(error);
+    throw new Error("Guest could not be updated");
+  }
+  revalidatePath("/account/profile");
 }
